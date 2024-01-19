f4145415d125065f5d50ab3dacabe7db3d89fab9
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2024-01-19 20:47:15 +0000

    final

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
-// app.js
-const bicicletasAPIRouter = require('./routes/api/bicicleta.js');
-const express = require('express');
+var createError = require('http-errors');
 
-let app = express();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var bicicletasAPIRouter = require('./routes/api/bicicleta');
+var app = express();
 
+// Configuracion en este caso de las vistas y el motor de las vistas(pug)
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Rutas
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/api/bicicleta', bicicletasAPIRouter);
-app.use('/api/bicicleta/create', bicicletasAPIRouter);
+// Manejador de errores
 
-module.exports = app;  // Asegúrate de exportar la aplicación
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// middleware para el manejo de errores
+app.use(function (err, req, res, next) {
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Rendereriza la pagina de error
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//export del server
+module.exports = app;
--- controllers/api/BicicletaController.js
+++ controllers/api/BicicletaController.js
@@ -1,4 +1,4 @@
-let Bicicleta = require('../../models/Bicicleta');
+let Bicicleta = require("../../models/Bicicleta");
 
 exports.bicicleta_list = function (req, res) {
     res.status(200).json({
@@ -20,4 +20,16 @@ exports.bicicleta_create = function (req, res) {
 exports.bicicleta_delete = function (req, res) {
     Bicicleta.removeById(req.body.id);
     res.status(204).send();
+};
+
+exports.bicicleta_update = function (req, res) {
+    let bici = Bicicleta.findById(req.body.id);
+    if (bici) {
+        bici.color = req.body.color;
+        bici.modelo = req.body.modelo;
+        bici.ubicacion = [req.body.latitud, req.body.longitud];
+        res.status(200).json({ bicicleta: bici });
+    } else {
+        res.status(404).send("No se encontró la bicicleta con ese ID");
+    }
 };
